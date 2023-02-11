31bbb76e109e2cab782ee7ad5f80b961174ed8eb
Carlos ivan <carlos.ivan10043@gmail.com>
2023-02-11 11:58:34 -0700

    new data structure

--- datos/PiecesBlack.ts
+++ datos/PiecesBlack.ts
@@ -1,10 +1,42 @@
-const piecesBlack = {
-    pawn: `https://www.chess.com/chess-themes/pieces/neo/150/bp.png`,
-    horse: `https://www.chess.com/chess-themes/pieces/neo/150/bn.png`,
-    bishop: `https://www.chess.com/chess-themes/pieces/neo/150/bb.png`,
-    tower: `https://www.chess.com/chess-themes/pieces/neo/150/br.png`,
-    queen: `https://www.chess.com/chess-themes/pieces/neo/150/bk.png`,
-    king: `https://www.chess.com/chess-themes/pieces/neo/150/bk.png`,
-};
+import { pieceChess } from '@interfaces/index';
+
+const piecesBlack:pieceChess[] = [
+    {
+        name: 'pawn',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/bp.png`,
+        color: 'black',
+        initialPosition: ['A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7'],
+    },
+    {
+        name: 'horse',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/bn.png`,
+        color: 'black',
+        initialPosition: ['B8', 'G8'],
+    },
+    {
+        name: 'bishop',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/bb.png`,
+        color: 'black',
+        initialPosition: ['C8', 'F8'],
+    },
+    {
+        name: 'tower',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/br.png`,
+        color: 'black',
+        initialPosition: ['A8', 'H8'],
+    },
+    {
+        name: 'queen',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/bq.png`,
+        color: 'black',
+        initialPosition: ['D8'],
+    },
+    {
+        name: 'king',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/bk.png`,
+        color: 'black',
+        initialPosition: ['E8'],
+    },
+];
 
 export default piecesBlack;
--- datos/PiecesWhite.ts
+++ datos/PiecesWhite.ts
@@ -1,10 +1,42 @@
-const piecesWhite = {
-    pawn: `https://www.chess.com/chess-themes/pieces/neo/150/wp.png`,
-    horse: `https://www.chess.com/chess-themes/pieces/neo/150/wn.png`,
-    bishop: `https://www.chess.com/chess-themes/pieces/neo/150/wb.png`,
-    tower: `https://www.chess.com/chess-themes/pieces/neo/150/wr.png`,
-    queen: `https://www.chess.com/chess-themes/pieces/neo/150/wq.png`,
-    king: `https://www.chess.com/chess-themes/pieces/neo/150/wk.png`,
-};
+import { pieceChess } from '@interfaces/index';
+
+const piecesWhite:pieceChess[] = [
+    {
+        name: 'pawn',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/wp.png`,
+        color: 'white',
+        initialPosition: ['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2'],
+    },
+    {
+        name: 'horse',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/wn.png`,
+        color: 'white',
+        initialPosition: ['B1', 'G1'],
+    },
+    {
+        name: 'bishop',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/wb.png`,
+        color: 'white',
+        initialPosition: ['C1', 'F1'],
+    },
+    {
+        name: 'tower',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/wr.png`,
+        color: 'white',
+        initialPosition: ['A1', 'H1'],
+    },
+    {
+        name: 'queen',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/wq.png`,
+        color: 'white',
+        initialPosition: ['D1'],
+    },
+    {
+        name: 'king',
+        path: `https://www.chess.com/chess-themes/pieces/neo/150/wk.png`,
+        color: 'white',
+        initialPosition: ['E1'],
+    },
+];
 
 export default piecesWhite;
