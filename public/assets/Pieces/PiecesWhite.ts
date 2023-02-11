a6f7943f875a33cf8aed10793b0c7d4c2d97f390
Carlos ivan <carlos.ivan10043@gmail.com>
2023-02-10 21:06:52 -0700

    Images with white pieces

---
+++ public/assets/Pieces/PiecesWhite.ts
@@ -0,0 +1,10 @@
+const piecesWhite = {
+    pawn: `https://www.chess.com/chess-themes/pieces/neo/150/wp.png`,
+    horse: `https://www.chess.com/chess-themes/pieces/neo/150/wn.png`,
+    bishop: `https://www.chess.com/chess-themes/pieces/neo/150/wb.png`,
+    tower: `https://www.chess.com/chess-themes/pieces/neo/150/wr.png`,
+    queen: `https://www.chess.com/chess-themes/pieces/neo/150/wq.png`,
+    king: `https://www.chess.com/chess-themes/pieces/neo/150/wk.png`,
+};
+
+export default piecesWhite;
