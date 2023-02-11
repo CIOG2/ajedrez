6966b29a4e0a2c964c08227c212e557e0aac1f0b
Carlos ivan <carlos.ivan10043@gmail.com>
2023-02-10 20:57:48 -0700

    Column component

---
+++ components/Board/Column/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import Styles from './styles.module.scss'
+
+interface Props{
+    letter: string;
+    index: number;
+}
+
+const Line:FC<Props> = ({letter, index}) =>{
+
+    const nameClass = (index % 2 === 0) ? 'par' : 'inpar'
+
+    return(
+        <section className = {`${Styles['section']} ${Styles[`${nameClass}`]}`}>
+            <div id={letter + 1} className = {`${Styles['cube']} ${Styles['point']}`}> {letter + 1} </div>
+            <div id={letter + 2} className = {`${Styles['cube']}`}> {letter + 2} </div>
+            <div id={letter + 3} className = {`${Styles['cube']}`}> {letter + 3} </div>
+            <div id={letter + 4} className = {`${Styles['cube']}`}> {letter + 4} </div>
+            <div id={letter + 5} className = {`${Styles['cube']}`}> {letter + 5} </div>
+            <div id={letter + 6} className = {`${Styles['cube']}`}> {letter + 6} </div>
+            <div id={letter + 7} className = {`${Styles['cube']}`}> {letter + 7} </div>
+            <div id={letter + 8} className = {`${Styles['cube']}`}> {letter + 8} </div>
+        </section>
+    )
+}
+
+export default Line;
