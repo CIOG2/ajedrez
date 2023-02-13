ad85ff95f41168e0853e63a743e97242c254557a
Carlos ivan <carlos.ivan10043@gmail.com>
2023-02-12 22:00:33 -0700

    add: button start

--- components/Board/index.tsx
+++ components/Board/index.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
+import piecesWhite from "@datos/PiecesWhite";
+import piecesBlack from "@datos/PiecesBlack";
+import game from 'utils/game';
 import Styles from './styles.module.scss'
 import Colums from './Column'
 
 const board = () =>{
     const letras = ['A','B','C','D','E','F','G','H'];
+    const dataPieces = [...piecesWhite, ...piecesBlack];
+    const newGame = new game();
+
 
     return(
         <main className = {`${Styles['main']}`}>
@@ -13,6 +20,13 @@ const board = () =>{
                     index={index}
                 />   
             )}
+            <button 
+                className = {`${Styles['button__start']}`}
+                onClick = {() => newGame.startGame(dataPieces)}
+                id = 'buttonStart'
+            >
+                Iniciar
+            </button>
         </main>
     )
 }
