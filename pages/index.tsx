f35c7910b7e505768000902aedbd8cab613109b1
Carlos ivan <carlos.ivan10043@gmail.com>
2023-02-13 22:03:17 -0700

    add: render board chess

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
+import Board from 'components/Board'
 
 
 const Home: NextPage = () => {
   return (
     <div id = 'APP'>
-
+      <Board/>
     </div>
   )
 }
