c2cfdc94a44a26f3a45316665b6d57f3d727e4f1
Carlos ivan <carlos.ivan10043@gmail.com>
2023-02-17 18:45:47 -0700

    add: new functions

--- Utils/pieceChess.ts
+++ Utils/pieceChess.ts
@@ -1,5 +1,3 @@
-import game from "./game";
-
 // import { pieceChess } from "@interfaces/pieceChess";
 interface constructorChess {
     name: string;
@@ -20,12 +18,25 @@ class ChessPiece{
 
     letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
-     constructor({name, path, color, position}: constructorChess) {
+    constructor({name, path, color, position}: constructorChess) {
         this.name = name;
         this.path = path;
         this.color = color;
         this.position = position;
     }
+
+
+    movePieceBoard = (position: string, nextPosition: string, generarPieza:any) => {
+        const piece = this.copyPieceVirtualBoard(position);
+        this.deletePieceDom(position);
+        this.position = nextPosition;
+        this.deletePieceVirtualBoard(position);
+        this.addPieceVirtualBoard(nextPosition, piece);
+        this.removePositionAvailable();
+        this.removePositionAvailableToEat();
+        generarPieza();
+    }
+
 
     deletePieceDom = (position: string) => {
         const element = document.getElementById(position)!;
@@ -33,11 +44,13 @@ class ChessPiece{
         if (child) child.remove();
     }
 
+
     deletePieceVirtualBoard = (position: string) => {
         const y = this.letterToNumber(position[0]);
         const x = parseInt(position[1]) - 1;
         this.virtualBoard[y][x] = 0;
     }
+
 
     addPieceVirtualBoard = (position: string, piece: {}) => {
         const y = this.letterToNumber(position[0]);
@@ -45,12 +58,14 @@ class ChessPiece{
         this.virtualBoard[y][x] = piece;
     }
 
+    
     copyPieceVirtualBoard = (position: string) => {
         const y = this.letterToNumber(position[0]);
         const x = parseInt(position[1]) - 1;
         return this.virtualBoard[y][x];
     }
 
+    
     removePositionAvailable = () => {
         const avalibleMoves = document.querySelectorAll('.position__available');
         
@@ -61,6 +76,7 @@ class ChessPiece{
         if (pieceSelet) pieceSelet.remove();
     }
     
+
     pieceSelected = (position: string) => {
         const piece = document.getElementById(position);
         const pieceSelected = document.createElement('div');
@@ -68,12 +84,35 @@ class ChessPiece{
         piece?.appendChild(pieceSelected);
     }
 
+
     removePositionAvailableToEat = () => {
         const avalibleMoves = document.querySelectorAll('.position__available--eat');
         
         if (avalibleMoves) 
             avalibleMoves.forEach((element) => element.remove());
     }
+
+
+    pieceInThisPosition = (position: string) => {
+        const y = this.letterToNumber(position[0]);
+        const x = parseInt(position[1]) - 1;
+
+        if (y < 0 || y > 7 || x < 0 || x > 7)
+            return false;
+
+        return (this.virtualBoard[y][x]) ? true : false;
+    }
+
+
+    pieceAvalibleToEat = (position: string) => {
+        if (this.pieceInThisPosition(position)) {
+            const y = this.letterToNumber(position[0]);
+            const x = parseInt(position[1]) - 1;
+            if (this.virtualBoard[y][x].color !== this.color)
+                this.positionAvailableEat.push(position);
+        }
+    }
+
     
     numberToLetter = (number: number) => this.letters[number];    
     
