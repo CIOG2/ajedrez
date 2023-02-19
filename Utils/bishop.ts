85dfbeab0b71af2be3d2bc4bda9978ffaf15ea6f
Carlos ivan <carlos.ivan10043@gmail.com>
2023-02-19 13:27:53 -0700

    class Bishop

---
+++ Utils/bishop.ts
@@ -0,0 +1,31 @@
+import pieceChess from "./pieceChess";
+
+class horse extends pieceChess{
+    
+    constructor({name, path, color, position}:any) { 
+        super({name, path, color, position});
+    }
+
+    generetePiece = () => {
+        const element = document.getElementById(this.position)!;  
+        const image = document.createElement('img');
+        image.src = this.path;
+        image.alt = `${this.name} ${this.color}`;
+        image.classList.add('image__piece');
+        image.addEventListener('click', () => this.previewMove());
+
+        element.innerHTML = '';      
+        element.appendChild(image);
+    }
+
+
+    previewMove = () => {
+        // this.removePositionAvailable();
+        // this.removePositionAvailableToEat();
+        // this.moveAvailable();
+        // this.showPositionAvailable();
+    }
+    
+}
+
+export default horse;
