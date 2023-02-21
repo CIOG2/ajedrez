5b12ff8607d4dcfe62a222fc2d0796f04f3731ae
Carlos ivan <carlos.ivan10043@gmail.com>
2023-02-20 18:32:18 -0700

    add: logic bishop ready

--- Utils/bishop.ts
+++ Utils/bishop.ts
@@ -1,6 +1,6 @@
 import pieceChess from "./pieceChess";
 
-class horse extends pieceChess{
+class bishop extends pieceChess{
     
     constructor({name, path, color, position}:any) { 
         super({name, path, color, position});
@@ -20,12 +20,52 @@ class horse extends pieceChess{
 
 
     previewMove = () => {
-        // this.removePositionAvailable();
-        // this.removePositionAvailableToEat();
-        // this.moveAvailable();
-        // this.showPositionAvailable();
+        this.removePositionAvailable();
+        this.removePositionAvailableToEat();
+        this.moveAvailable();
+        this.showPositionAvailable();
+    }
+
+    moveAvailable = () => {    
+        this.pieceSelected(this.position);
+        this.diagonalMove(this.position, this.color);
+        this.showPositionAvailableToEat();
     }
     
+
+    showPositionAvailableToEat = () => {
+        this.positionAvailableEat.forEach((posicionAvalible) => {
+            const element = document.getElementById(posicionAvalible)!;
+            const child = element.querySelector('.position__available--eat');
+
+            if (!child){     
+                const div = document.createElement('div');
+                div.classList.add('position__available--eat');
+                div.addEventListener('click', () => {
+                    this.movePieceBoard(this.position, posicionAvalible, this.generetePiece);  
+                });
+    
+                element.appendChild(div);
+            }    
+        });
+        this.positionAvailableEat = [];
+    }
+
+    showPositionAvailable = () => {
+        this.positionAvailable.forEach((posicionAvalible) => {
+            const element = document.getElementById(posicionAvalible)!;
+            const div = document.createElement('div');
+            div.classList.add('position__available');
+            div.addEventListener('click', () => {
+                this.movePieceBoard(this.position, posicionAvalible, this.generetePiece);  
+            });
+
+            element.innerHTML = '';
+            element.appendChild(div);
+        });
+    }
+
+
 }
 
-export default horse;
+export default bishop;
