4191f452586cf56d99f1842137ac0d04b913a3c3
Carlos ivan <carlos.ivan10043@gmail.com>
2023-02-26 21:58:08 -0700

    add: numbers board

--- components/Board/index.tsx
+++ components/Board/index.tsx
@@ -3,7 +3,7 @@ import piecesWhite from "@datos/PiecesWhite";
 import piecesBlack from "@datos/PiecesBlack";
 import game from 'utils/game';
 import Styles from './styles.module.scss'
-import Colums from './Column'
+import Columns from './Column'
 
 const board = () =>{
     const letras = ['A','B','C','D','E','F','G','H'];
@@ -14,12 +14,34 @@ const board = () =>{
     return(
         <main className = {`${Styles['main']}`}>
             {letras.map((item, index) =>
-                <Colums
+                <Columns
                     key={index}
                     letter={item}
                     index={index}
                 />   
             )}
+            <div className={`${Styles['main__simbol']} ${Styles['column']}`}>
+                <span>8</span> 
+                <span>7</span> 
+                <span>6</span>  
+                <span>5</span>  
+                <span>4</span>  
+                <span>3</span>  
+                <span>2</span>  
+                <span>1</span>
+            </div>
+            
+            <div className={`${Styles['main__simbol']} ${Styles['row']}`}>
+                <span>A</span> 
+                <span>B</span>
+                <span>C</span>
+                <span>D</span>
+                <span>E</span>
+                <span>F</span>
+                <span>G</span>
+                <span>H</span>
+            </div>
+
             <button 
                 className = {`${Styles['button__start']}`}
                 onClick = {() => newGame.startGame(dataPieces)}
