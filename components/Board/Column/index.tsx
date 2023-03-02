5f442b0a600c9e3cebd6cc5f32084466f23a53d8
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-01 23:20:13 -0700

    fix: remove numbers

--- components/Board/Column/index.tsx
+++ components/Board/Column/index.tsx
@@ -6,22 +6,22 @@ interface Props{
     index: number;
 }
 
-const Line:FC<Props> = ({letter, index}) =>{
+const Columns:FC<Props> = ({letter, index}) =>{
 
     const nameClass = (index % 2 === 0) ? 'par' : 'inpar'
 
     return(
         <section className = {`${Styles['section']} ${Styles[`${nameClass}`]}`}>
-            <div id={letter + 8} className = {`${Styles['cube']}`}> {letter + 8} </div>
-            <div id={letter + 7} className = {`${Styles['cube']}`}> {letter + 7} </div>
-            <div id={letter + 6} className = {`${Styles['cube']}`}> {letter + 6} </div>
-            <div id={letter + 5} className = {`${Styles['cube']}`}> {letter + 5} </div>
-            <div id={letter + 4} className = {`${Styles['cube']}`}> {letter + 4} </div>
-            <div id={letter + 3} className = {`${Styles['cube']}`}> {letter + 3} </div>
-            <div id={letter + 2} className = {`${Styles['cube']}`}> {letter + 2} </div>
-            <div id={letter + 1} className = {`${Styles['cube']}`}> {letter + 1} </div>
+            <div id={letter + 8} className = {`${Styles['cube']}`}>  </div>
+            <div id={letter + 7} className = {`${Styles['cube']}`}>  </div>
+            <div id={letter + 6} className = {`${Styles['cube']}`}>  </div>
+            <div id={letter + 5} className = {`${Styles['cube']}`}>  </div>
+            <div id={letter + 4} className = {`${Styles['cube']}`}>  </div>
+            <div id={letter + 3} className = {`${Styles['cube']}`}>  </div>
+            <div id={letter + 2} className = {`${Styles['cube']}`}>  </div>
+            <div id={letter + 1} className = {`${Styles['cube']}`}>  </div>
         </section>
     )
 }
 
-export default Line;
+export default Columns;
