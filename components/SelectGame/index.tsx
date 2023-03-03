1fffe5fe66dc5ccf638fac008835ac7f5144dbf4
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-02 23:58:08 -0700

    SelectGame component

---
+++ components/SelectGame/index.tsx
@@ -0,0 +1,11 @@
+import Styles from './styles.module.scss'
+
+const SelectGame = () => {
+    return(
+        <main className = {`${Styles['main']}`}>
+            <h1>Selecciona un modo de juego</h1>
+        </main>
+    )
+}
+
+export default SelectGame;
