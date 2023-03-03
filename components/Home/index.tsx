b05b4f8853db10043bdf058f37144f97908d6240
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-02 23:40:37 -0700

    Home

---
+++ components/Home/index.tsx
@@ -0,0 +1,9 @@
+import Styles from './styles.module.scss'
+
+const Home = () => {
+    return(
+        <main className = {`${Styles['main']}`}>
+            <h1>Home</h1>
+        </main>
+    )
+}
