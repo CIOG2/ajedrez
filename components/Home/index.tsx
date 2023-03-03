9e64848a6481d86104dee0b658549c44f27f97dc
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-02 23:57:20 -0700

    delete

--- components/Home/index.tsx
+++
@@ -1,9 +0,0 @@
-import Styles from './styles.module.scss'
-
-const Home = () => {
-    return(
-        <main className = {`${Styles['main']}`}>
-            <h1>Home</h1>
-        </main>
-    )
-}
