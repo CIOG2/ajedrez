37465d2ec653c0e28e249ac686e2e34b0b332a0c
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-05 23:35:31 -0700

    select mode game

--- components/SelectGame/index.tsx
+++ components/SelectGame/index.tsx
@@ -1,9 +1,41 @@
 import Styles from './styles.module.scss'
 
 const SelectGame = () => {
+
+    const links = [
+        {
+            name: 'JUEGO EN LOCAL',
+            href: './localgame'
+        },
+        {
+            name: 'JUEGO EN LÍNEA',
+            href: './onlinegame'
+        },
+        {
+            name: 'VER PARTIDA',
+            href: './watchgame'
+        }
+    ];
+
+
     return(
         <main className = {`${Styles['main']}`}>
-            <h1>Selecciona un modo de juego</h1>
+            <h1
+                className = {`${Styles['main__title']}`}
+            >
+                Selecciona un modo de juego
+            </h1>
+
+            {links.map((link, index) => 
+                <a
+                    key = {index}
+                    href = {link.href}
+                    className = {`${Styles['main__button']}`}
+                >
+                    {link.name}
+                </a>
+            )}
+        
         </main>
     )
 }
