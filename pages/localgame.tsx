5d768863708098f80a711a8bccafdb511939839a
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-05 23:35:06 -0700

    new page

---
+++ pages/localgame.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Board from 'components/Board'
+
+const Home: NextPage = () => {
+  return (
+    <div id = 'APP'>
+        <Board/>
+    </div>
+  )
+}
+
+export default Home
