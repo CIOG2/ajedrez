dd72e877bbc82de9568feaf3650886dd4760e904
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-06 12:36:13 -0700

    class king

---
+++ Utils/king.ts
@@ -0,0 +1,38 @@
+import pieceChess from "./pieceChess";
+
+class king extends pieceChess{
+    
+    constructor({name, path, color, position}:any) { 
+        super({name, path, color, position});
+    }
+
+    generetePiece = () => {
+        const element = document.getElementById(this.position)!;  
+        const image = document.createElement('img');
+        image.src = this.path;
+        image.alt = `${this.name} ${this.color}`;
+        image.classList.add('image__piece');
+        image.addEventListener('click', () => this.previewMove());
+
+        element.innerHTML = '';      
+        element.appendChild(image);
+    }
+
+
+    previewMove = () => {
+        this.removePositionAvailable();
+        this.removePositionAvailableToEat();
+        this.moveAvailable();
+        this.showPositionAvailable(this.generetePiece);
+        this.positionAvailable = [];
+    }
+
+    moveAvailable = () => {    
+        this.pieceSelected(this.position);
+//        this.movePiece();
+        this.showPositionAvailableToEat(this.generetePiece);
+    }
+
+}
+
+export default king;
