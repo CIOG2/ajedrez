f668eddb706095a16655e4f1d68749cb0a2089ae
Carlos ivan <carlos.ivan10043@gmail.com>
2023-04-24 17:08:56 -0700

    Se restructoro la manera dinamica en la que se asigna classe para evitar conflictos con 'babel'

--- Utils/game.ts
+++ Utils/game.ts
@@ -7,12 +7,30 @@ import king from './king';
 import { pieceChess } from '@interfaces/index';
 
 const piecesChess = [
-    pawn,
-    horse,
-    bishop,
-    tower,
-    queen,
-    king,
+    {
+        name: 'pawn',
+        class: pawn,
+    },
+    {
+        name: 'horse',
+        class: horse,
+    },
+    {
+        name: 'bishop',
+        class: bishop,
+    },
+    {
+        name: 'tower',
+        class: tower,
+    },
+    {
+        name: 'queen',
+        class: queen,
+    },
+    {
+        name: 'king',
+        class: king,
+    },
 ]
 
 class game{
@@ -29,27 +47,24 @@ class game{
 
     letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
-    startGame(data:pieceChess[]) {
+    startGame = (data:pieceChess[]) => {
         data.map((piece) => {
             piece.initialPosition.map((position) => {
                 this.assignPosition(piece, position);            
             });
         });
-        
+
         const button = document.querySelector('#buttonStart');
         button?.remove();    
     }
 
-
-    assignPosition({name, path, color}:pieceChess, position:string) {
-        
+    assignPosition = ({name, path, color}:pieceChess, position:string) => {
         const y = this.letterToNumber(position[0]);
         const x = parseInt(position[1]) - 1;     
 
-
-        const classConstructor = piecesChess.filter((piece: Function) => piece.name === name)[0];
+        // Generar pieza con su respectiva clase de manera dinamica (postada: cuidado con babel 😢)
+        const { class: classConstructor } = piecesChess.filter((piece) => piece.name === name)[0];
         const piece = new classConstructor({name, path, color, position})
-
 
         if (piece) {
             piece.generetePiece();
